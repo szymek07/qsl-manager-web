da977a61b69f2e4b4df076bff71504bddbdc6a5e
szymek07 <szymek07@gmail.com>
2025-01-21 00:03:29 +0100

    add information about serviced callsign
    add github action to build image

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
@@ -12,9 +12,14 @@ import { ApiResponse } from './response.model';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
+  isSmallScreen: boolean = false;
+  mediaQueryList!: MediaQueryList;
+
   call: string = '';
   submitted: boolean = false;
+  checked: boolean = false;
+  serviced: boolean = false;
   apiResponse: ApiResponse[] = [];
   loading: boolean = false;
 
@@ -23,19 +28,29 @@ export class AppComponent {
   onSubmit(form: any) {
     console.log('Form submitted:', form.value);
     this.submitted = true;
+    this.checkCall(this.call);
 
-    this.makeApiCall(this.call);
 
   }
 
-  makeApiCall(callValue: string) {
-    const url = `/qsl/to-pickup?call=${callValue}`;
+  resetSubmitted(): void {
+    console.log('Resetting Form');
+    this.submitted = false;
+  }
+
+  checkCall(callValue: string) {
+    const url = `/qsl/is-call-serviced?call=${callValue}`;
     this.loading = true;
-    this.http.get<ApiResponse[]>(url).subscribe(
+    this.http.get<boolean>(url).subscribe(
       response => {
-        this.apiResponse = response;
+        this.checked = true;
+        this.serviced = response;
         this.loading = false;
-        console.log('API response:', response);
+        console.log('Check serviced response:', response);
+
+        if (response) {
+          this.toPickup(callValue);
+        }
       },
       error => {
         this.loading = false;
@@ -44,5 +59,35 @@ export class AppComponent {
     );
   }
 
-  title = 'qsl-manager-web';
+  toPickup(callValue: string) {
+    const url = `/qsl/to-pickup?call=${callValue}`;
+    this.loading = true;
+    this.http.get<ApiResponse[]>(url).subscribe(
+      response => {
+        this.apiResponse = response;
+        this.loading = false;
+        console.log('To pickup response:', response);
+      },
+      error => {
+        this.loading = false;
+        console.error('API error:', error);
+      }
+    );
+  }
+
+  ngOnInit(): void {
+    this.mediaQueryList = window.matchMedia('(max-width: 800px)');
+    this.isSmallScreen = this.mediaQueryList.matches;
+    this.mediaQueryList.addEventListener('change', this.handleScreenChange);
+
+  }
+
+  ngOnDestroy(): void {
+    this.mediaQueryList.removeEventListener('change', this.handleScreenChange);
+  }
+
+  handleScreenChange = (event: MediaQueryListEvent): void => {
+    this.isSmallScreen = event.matches;
+  };
+
 }
